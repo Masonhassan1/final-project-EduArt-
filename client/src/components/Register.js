6ad1526299d5fbbbd0c91148c2a4633fbf169b54
masonhassan1 <masonhassan397@gmail.com>
2016-01-01 12:00:00 +0500

    reg-site

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -7,21 +7,26 @@ function Register() {
   const [isLoading,setIsLoading] = useState(false)
 
   const formEl = useRef(null);
+  const firstNameEl = useRef(null); 
+  const lastNameEl = useRef(null); 
+  const userNameEl = useRef(null); 
   const birthdayEl = useRef(null)
-  const nameEl = useRef(null); 
   const emailEl = useRef(null); 
   const passwordEl = useRef(null); 
   async function submitHandler(e){
   
     const userData ={
-      name: nameEl.current.value,
+      firstName: firstNameEl.current.value,
+      lastName:lastNameEl.current.value,
+      userName: userNameEl.current.value,
       dateOfBirth:birthdayEl.current.value,
       eMail: emailEl.current.value,
       password: passwordEl.current.value
     }
+    console.log(userData)
     try {
       setIsLoading(true);
-      const axiosResp = await axios.post("http://localhost:3000/user", userData);
+      const axiosResp = await axios.post("http://localhost:4000/user", userData);
       setIsLoading(false);
       if(axiosResp.data.error) {
        console.log(axiosResp.data.error)
@@ -44,8 +49,11 @@ function Register() {
 
     <form ref={formEl} className='reg-form' action="">
      <div>Register</div>
-     <input ref={nameEl} type="text"  placeholder='User name'/>
-     <input ref={birthdayEl} type="text" placeholder='Birthday'/>
+     <br />
+     <input ref={firstNameEl} type="text"  placeholder='First name'/>
+     <input ref={lastNameEl} type="text"  placeholder='Last name'/>
+     <input ref={userNameEl} type="text"  placeholder='User name'/>
+     <input ref={birthdayEl} type="text" placeholder='Birthday dd.mm.yy'/>
      <input ref={emailEl} type="email"  placeholder='Email'/>
      <input ref={passwordEl} type="password"  placeholder='Password'/>
      <button className='register-btn' onClick={submitHandler}>Register</button>
