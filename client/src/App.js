6a282c245789da0da53251c3031d21db051342bc
masonhassan1 <masonhassan397@gmail.com>
2016-01-01 12:00:00 +0500

    feat: create Course List page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button} from "react-bootstrap"
+import "./App.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
+import CourseList from "./components/CourseList/CourseList";
+import { Button } from "react-bootstrap";
 function App() {
   return (
     <div className="App">
-      <h1>React App Template</h1>
-      <Button variant="success">Success</Button>
+      {/*      <h1>React App Template</h1>
+      <Button variant="success">Success</Button> */}
+      <CourseList />
     </div>
   );
 }
---
+++ client/src/components/CourseList/CourseList.js
@@ -0,0 +1,67 @@
+import React from "react";
+import "./CourseList.css";
+import { GoSearch } from "react-icons/go";
+
+const courseArr = [
+  {
+    name: "Web Development",
+    duration: "12 months",
+    start: "01.02.2023",
+    id: 1,
+    src: "https://cdn.dribbble.com/users/1270214/screenshots/5646236/media/ef6ec26746b87842e916e5c0f5b6ec27.png?compress=1&resize=800x600&vertical=top",
+    text: "With our course, you will learn how to create dynamic websites and interactive web applications.",
+  },
+  {
+    name: "Web Design",
+    duration: "6 months",
+    start: "05.03.2023",
+    id: 2,
+    src: "https://thumbs.dreamstime.com/z/developers-create-internet-app-software-engineering-computer-technology-website-development-web-application-coding-design-web-150140993.jpg",
+    text: "Students will gain the skills and project-based experience needed for entry into web design and development careers.",
+  },
+  {
+    name: "Web Development",
+    duration: "12 months",
+    start: "08.04.2023",
+    id: 3,
+    src: "https://cdn.dribbble.com/users/1270214/screenshots/5646236/media/ef6ec26746b87842e916e5c0f5b6ec27.png?compress=1&resize=800x600&vertical=top",
+    text: "With our course, you will learn how to create dynamic websites and interactive web applications.",
+  },
+  {
+    name: "Web Design",
+    duration: "6 months",
+    start: "11.05.2023",
+    id: 4,
+    src: "https://thumbs.dreamstime.com/z/developers-create-internet-app-software-engineering-computer-technology-website-development-web-application-coding-design-web-150140993.jpg",
+    text: "Students will gain the skills and project-based experience needed for entry into web design and development careers.",
+  },
+];
+
+export default function CourseList() {
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+  };
+  return (
+    <div>
+      <form className="search-form" onSubmit={(e) => submitForm(e)}>
+        <GoSearch className="search-icon" />
+        <input type="text" name="search" placeholder="Search" />
+      </form>
+      <section className="courseListSection">
+        {courseArr.map((course, id) => {
+          return (
+            <div key={id} className="courseCard">
+              <img src={course.src} alt={course.name} />
+              <h3 className="courseNameCard">{course.name}</h3>
+              <h5 className="courseDurationCard">{course.duration}</h5>
+              <h5 className="courseStartCard">Start: {course.start}</h5>
+              <p className="courseTextCard">{course.text}</p>
+              <button className="viewCourseBtn">View course</button>
+            </div>
+          );
+        })}
+      </section>
+    </div>
+  );
+}
